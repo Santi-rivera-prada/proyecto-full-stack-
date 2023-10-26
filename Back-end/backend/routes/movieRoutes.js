f9494bde17914d50e6736594b08bd0ce48b728d1
santi <120440622+Santi-rivera-prada@users.noreply.github.com>
2023-10-25 23:50:36 -0500

    finish

--- Back-end/backend/routes/movieRoutes.js
+++ Back-end/backend/routes/movieRoutes.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const movieController = require('../controllers/movieControllers');
-//const authenticate = require('../middleware/authMiddleware');
+const authenticate = require('../middleware/authMiddleware');
 
 // Ruta para crear una nueva película
-router.post('/movies', movieController.createMovie);
+router.post('/movies',authenticate, movieController.createMovie);
 
 // Ruta para obtener todas las películas
 router.get('/movies', movieController.getMovies);
@@ -13,10 +13,10 @@ router.get('/movies', movieController.getMovies);
 router.get('/movies/:id', movieController.getMovies);
 
 // Ruta para actualizar una película por su ID
-router.put('/movies/:id', movieController.updateMovie);
+router.put('/movies/:id',authenticate, movieController.updateMovie);
 
 // Ruta para eliminar una película por su ID
-router.delete('/movies/:id', movieController.deleteMovie);
+router.delete('/movies/:id',authenticate, movieController.deleteMovie);
 
 
 module.exports = router;
