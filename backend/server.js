d642d0b3e4a6fa5d2fd9987efcdbdacefc9fda45
santi <120440622+Santi-rivera-prada@users.noreply.github.com>
2023-11-07 22:45:47 -0500

    ..

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-mongoose.connect(process.env.MONGODB_URI, {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
