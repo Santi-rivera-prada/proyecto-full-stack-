27e1802685bbbf88df85239dad0639da6af618ff
santi <120440622+Santi-rivera-prada@users.noreply.github.com>
2023-11-21 00:54:55 -0500

    cors

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const cors = require('cors')
 
 dotenv.config();
 
@@ -25,9 +26,12 @@ const userRoutes = require('./routes/usersRoutes');
 app.use('/api', movieRoutes);
 app.use('/api', userRoutes);
 
+
 // Middleware de manejo de errores (debe ir al final)
 const errorMiddleware = require('./middleware/errorMiddleware');
 app.use(errorMiddleware);
+
+app.use(cors());
 
 app.listen(PORT, () => {
   console.log(`Servidor en funcionamiento en el puerto ${PORT}`);
