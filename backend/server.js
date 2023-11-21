621d7900605f59d319fbb14bf83a160d46791658
santi <120440622+Santi-rivera-prada@users.noreply.github.com>
2023-11-21 01:15:23 -0500

    cors general

--- backend/server.js
+++ backend/server.js
@@ -33,7 +33,13 @@ app.use(errorMiddleware);
 
 app.use(cors());
 
-app.use('/api/movies', movieRoutes);
+const corsOptions = {
+  origin: 'http://localhost:5173', // local del frontend general de practica 
+  methods: 'GET,PUT,POST,DELETE',
+  allowedHeaders: 'Content-Type,Authorization',
+};
+
+app.use(cors(corsOptions));
 
 app.listen(PORT, () => {
   console.log(`Servidor en funcionamiento en el puerto ${PORT}`);
